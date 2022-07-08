474fb1f8c9174b572de79d93d5c7b3701dd41dec
Elena Gracheva <elena.gracheva.639@gmail.com>
2022-07-08 22:06:45 +0500

    added first screen component

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const FilmInfo = {
+  TITLE: 'The Grand Budapest Hotel',
+  GENRE: 'Drama',
+  YEAR: 2014
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App title={FilmInfo.TITLE} genre={FilmInfo.GENRE} year={FilmInfo.YEAR} />
   </React.StrictMode>,
 );
