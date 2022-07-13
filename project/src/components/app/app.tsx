50c680be7f7f6acb15ce9775c267915b6a628852
Elena Gracheva <elena.gracheva.639@gmail.com>
2022-07-13 19:30:02 +0500

    added routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
 import FirstScreen from '../pages/first-screen/first-screen';
+import AuthScreen from '../pages/auth-screen/auth-screen';
+import FilmsListScreen from '../pages/films-list-screen/films-list-screen';
+import FilmScreen from '../pages/film-screen/film-screen';
+import AddReviewScreen from '../pages/add-review-screen/add-review-screen';
+import PlayerScreen from '../pages/player-screen/player-screen';
+import NotFoundScreen from '../pages/not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   title: string,
@@ -6,9 +15,44 @@ type AppScreenProps = {
   year: number
 }
 
-function App({title, genre, year}: AppScreenProps): JSX.Element {
+function App({ title, genre, year }: AppScreenProps): JSX.Element {
   return (
-    <FirstScreen title={ title } genre={ genre } year={ year } />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={ AppRoute.Root }
+          element={ <FirstScreen title={ title } genre={ genre } year={ year } /> }
+        />
+        <Route
+          path={ AppRoute.Login }
+          element={ <AuthScreen /> }
+        />
+        <Route
+          path={ AppRoute.FilmsList }
+          element={
+            <PrivateRoute authorizationStatus={ AuthorizationStatus.NoAuth } >
+              <FilmsListScreen />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={ AppRoute.Film }
+          element={ <FilmScreen title={ title } genre={ genre } year={ year } /> }
+        />
+        <Route
+          path={ AppRoute.AddReview }
+          element={ <AddReviewScreen /> }
+        />
+        <Route
+          path={ AppRoute.Player }
+          element={ <PlayerScreen /> }
+        />
+        <Route
+          path="*"
+          element={ <NotFoundScreen /> }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- project/src/components/pages/not-found-screen/not-found-screen.tsx
+++ project/src/components/pages/not-found-screen/not-found-screen.tsx
@@ -1,18 +1,22 @@
 import Footer from '../../footer/footer';
 import Header from '../../header/header';
+import { Link } from 'react-router-dom';
 
 function NotFoundScreen(): JSX.Element {
   return (
     <div className="not-found-page">
+      <div className="page-content">
+        <Header isAuthorized={ false } />
 
-      <Header isAuthorized={ false } />
+        <div className="not-found-page__content" style={{ marginBottom: '40px', minHeight: 'calc(100vh - 336px)' }}>
+          <p style={{ textAlign: 'center' }}>404 - page not found</p>
+          <Link className="" to='/' style={{ textAlign: 'center', display: 'block', color: '#c9b37e' }}>
+            Return to main page
+          </Link>
+        </div>
 
-      <div className="not-found-page__content">
-        <p>404 - page not found</p>
-        <a href="main.html" className="logo__link logo__link--light">Return to main page</a>
+        <Footer />
       </div>
-
-      <Footer />
     </div>
   );
 }
